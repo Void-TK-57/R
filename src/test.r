da8812f733303f5b88f1a1bbbd6fd455ecc03a23
Void <pedroaa57@gmail.com>
2019-11-02 21:48:36 -0300

    Add Arguments

--- src/test.r
+++ src/test.r
@@ -1,2 +1,25 @@
-mydata <- read.table("../data/regression.csv", header=TRUE, sep=",")
-print(typeof(mydata) )
+
+# main function
+main <- function(path, dependent_value, degree=2) {
+    # change path based on data folder
+    path <- paste("../data/", path, sep="")
+    # read table
+    data_ <- read.table(path, , header=TRUE, sep=",")
+    # create model
+    model <- lm("Rendimento ~ Temperatura", data=data_)
+    # print summary
+    print(summary(model))
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+# check args
+print(args)
+if (length(args) == 0) {
+    print("[Error]: No arguments passed")
+} else if (length(args) == 1) {
+    print("[Error]: Only one argument passed")
+} else if (length(args) == 2){
+    main(args[1], args[2])
+} else {
+    main(args[1], args[2], arg[3])
+}
